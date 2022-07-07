e73dbaddf5f8a31f2ecbfa878fef93330b5d04fb
sujin <atena9604@naver.com>
2022-07-07 23:47:11 +0900

    [feat] 입력 받는 text 박스 추가

---
+++ src/components/InputBox.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hello">
+    <textarea v-model="item"/> <button @click="onSave()">저장</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputBox',
+  data() {
+    return{
+      item: "",
+  }
+},
+  methods: {
+    onSave() {
+      /*Todo axios를 이용하여 db에 값을 넣고 리스트로 보여주는 기능 개발*/
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <InputBox/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import InputBox from '@/components/InputBox.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    InputBox
   }
 }
 </script>
