1d621c5017a69bd9f0b4898255a51ac758833b68
sujin <atena9604@naver.com>
2022-07-19 00:35:14 +0900

    [feat] db에 있는값 보여주도록 수정

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,41 @@
 <template>
   <div class="home">
     <InputBox/>
+    <ul>
+      <li>test</li>
+      <li v-for="item in items" :key="item.contentKey">
+        {{ item.content }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import InputBox from '@/components/InputBox.vue'
+import axios from "axios";
 
 export default {
   name: 'HomeView',
   components: {
     InputBox
+  },
+  data (){
+    return{
+      items : [],
+    }
+  },
+  mounted() {
+    axios.get('http://localhost:8081/getList')
+        .then(res => {
+          console.log(res, res.data);
+          this.items = res.data;
+          console.log(this.items);
+        })
+  },
+  methods: {
+
   }
+
 }
 </script>
